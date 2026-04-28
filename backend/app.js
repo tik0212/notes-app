0469536175e9ae3119748ece1b3d150caae1bc2b
Tikhacheva <tikhacheva07@gmail.com>
2026-04-28 15:48:23 +0300

    feat: complete add.js with successful mongodb connection

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-const morgan = require("morgan");
+// const morgan = require("morgan");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("🍃 MongoDB подключена успешно!"))
+  .catch((err) => console.log("❌ Ошибка подключения к MongoDB:", err));
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("hi");
